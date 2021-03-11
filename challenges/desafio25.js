ccbd1ffb13697c327187bf4c2ca597224db97cc3
Felipe Nascimento <flpnascto@gmail.com>
2021-03-11 13:31:32 -0300

    Desafio 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,20 @@
+db.voos
+  .deleteMany(
+    {
+      $and:
+        [
+          {
+            litrosCombustivel: { $lt: 400 },
+          },
+          {
+            "empresa.nome": "AZUL",
+          },
+        ],
+    },
+    {
+      _id: 0,
+      vooId: 1,
+      "empresa.nome": 1,
+      litrosCombustivel: 1,
+    },
+  );
